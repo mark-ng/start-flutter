f90f67cb686742a7e3af52d4a641cf068619bcc0
Mark Ng <markng12345678912@gmail.com>
2021-08-01 13:41:14 +0800

    Added style and font family

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,23 @@ void main() {
       appBar: AppBar(
         title: Text("Hello, I am mark"),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('I am the body'),
+        child: Text(
+          'I am the body',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower'
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => 1,
         child: Text("click"),
+        backgroundColor: Colors.red[600],
       ),
     )
   ));
