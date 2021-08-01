1e786b6a8e0d7b5e7d9187431e3698f50698940a
Mark Ng <markng12345678912@gmail.com>
2021-08-01 14:42:16 +0800

    Use StatelessWidget to enable hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, I am mark"),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: Text(
-          'I am the body',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => 1,
-        child: Text("click"),
-        backgroundColor: Colors.red[600],
-      ),
-    )
-  ));
+  runApp(Home());
 }
 
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello, I am mark"),
+            centerTitle: true,
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Text(
+              'I am the body',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                  fontFamily: 'IndieFlower'
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => 1,
+            child: Text("click"),
+            backgroundColor: Colors.red[600],
+          ),
+        )
+    );
+  }
+}
