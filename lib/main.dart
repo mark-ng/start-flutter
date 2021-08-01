40cd1db3e5c64559bd3712b5ac2948cbe675a8cd
Mark Ng <markng12345678912@gmail.com>
2021-08-01 13:21:14 +0800

    Added Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('hello world'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, I am mark"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('I am the body'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => 1,
+        child: Text("click"),
+      ),
+    )
   ));
 }
 
